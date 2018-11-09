15b7624fd093a4adb635e6ba64abba0cce95f25f
Jakub Kubacki <kubacki.jk@gmail.com>
2018-11-10 02:01:27 +0700

    Add social media profiles

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,10 @@
+# frozen_string_literal: true
+
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :confirmable
+
+  has_many :social_media_profiles
 end
