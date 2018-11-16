888d27058b1c1d618afcde0d8c8c5bb84f021dde
Jakub Kubacki <kubacki.jk@gmail.com>
2018-11-16 23:38:51 +0700

    Change email confirmation path

--- app/mailers/users/confirmation_mailer.rb
+++ app/mailers/users/confirmation_mailer.rb
@@ -5,7 +5,7 @@ module Users
     def send_confirmation(user_id)
       @user = User.find(user_id)
       @confirmation_url =
-        "#{Rails.application.credentials.frontend_host}/confirm?" \
+        "#{Rails.application.credentials.frontend_host}/confirm-email?" \
         "confirmation_token=#{@user.confirmation_token}"
       mail(to: @user.email, subject: "Confirm your account")
     end
