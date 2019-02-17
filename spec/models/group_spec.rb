d5f4dbe971f979ac25a5225a56f4e9b337aca92d
Jakub Kubacki <kubacki.jk@gmail.com>
2019-02-17 14:02:25 -0500

    Fix rubocop config and issues

--- spec/models/group_spec.rb
+++ spec/models/group_spec.rb
@@ -1,4 +1,6 @@
-require 'rails_helper'
+# frozen_string_literal: true
+
+require "rails_helper"
 
 RSpec.describe Group, type: :model do
   it { is_expected.to validate_presence_of :meeting_length_in_minutes }
