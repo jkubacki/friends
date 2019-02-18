7a214e497501c72aa8ab68aa1090cbf9c98c2f68
Jakub Kubacki <kubacki.jk@gmail.com>
2019-02-18 02:00:07 -0500

    FreeWeekendSlot > FreeWeekendSlots

--- app/services/scheduling/free_weekend_slots.rb
+++ app/services/scheduling/free_weekend_slots.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 module Scheduling
-  class FreeWeekendSlot < ApplicationService
+  class FreeWeekendSlots < ApplicationService
     def initialize(group:)
       @group = group
     end
