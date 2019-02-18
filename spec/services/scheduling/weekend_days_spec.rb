d478a5242ead969ff5858f2f7be29f592130333f
Jakub Kubacki <kubacki.jk@gmail.com>
2019-02-18 13:01:22 -0500

    Fix codestyle issues

--- spec/services/scheduling/weekend_days_spec.rb
+++ spec/services/scheduling/weekend_days_spec.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe Scheduling::WeekendDays do
   include ActiveSupport::Testing::TimeHelpers
@@ -8,12 +8,13 @@ RSpec.describe Scheduling::WeekendDays do
   subject { described_class.call }
 
   before { travel_to Time.zone.local(2019, 2, 18, 0, 0) }
+
   after { travel_back }
 
-  let(:saturday) { '2019-02-23'.to_date }
-  let(:sunday) { '2019-02-24'.to_date }
+  let(:saturday) { "2019-02-23".to_date }
+  let(:sunday) { "2019-02-24".to_date }
 
-  it 'returns array with this saturday and sunday' do
+  it "returns array with this saturday and sunday" do
     expect(subject).to eq [saturday, sunday]
   end
 end
