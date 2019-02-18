1268073b4148de846a5d5dc332e1acf43aca3123
Jakub Kubacki <kubacki.jk@gmail.com>
2019-02-18 01:04:38 -0500

    Add Scheduling::SubtractTimeRanges

---
+++ app/services/scheduling/subtract_time_ranges.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+module Scheduling
+  class SubtractTimeRanges < ApplicationService
+    def initialize(range:, subtract_time_ranges:)
+      @range = range
+      @subtract_time_ranges = subtract_time_ranges
+    end
+
+    def call
+      available_timeranges = [range]
+
+      subtract_time_ranges.each do |subtract|
+        available_timeranges = available_timeranges.inject([]) do |timeranges, timerange|
+          timeranges + Scheduling::SubtractTimeRange.call(range: timerange, subtract: subtract)
+        end
+      end
+
+      available_timeranges
+    end
+
+    private
+
+    attr_reader :range, :subtract_time_ranges
+  end
+end
