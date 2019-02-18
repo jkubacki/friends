62c1acc5e9f97a9b011b5e19f96866da9f84b656
Jakub Kubacki <kubacki.jk@gmail.com>
2019-02-17 23:25:01 -0500

    Add SubtractTimeRange service

---
+++ app/services/scheduling/subtract_time_range.rb
@@ -0,0 +1,37 @@
+# frozen_string_literal: true
+
+module Scheduling
+  class SubtractTimeRange < ApplicationService
+    def initialize(range:, subtract:)
+      @range = range
+      @subtract = subtract
+    end
+
+    def call
+      return range unless range.overlaps?(subtract)
+      return nil if subtract.include?(range)
+
+      if range.include?(subtract)
+        split_time_range
+      else
+        trim_time_range
+      end
+    end
+
+    private
+
+    def split_time_range
+      [range.first..subtract.first, subtract.last..range.last]
+    end
+
+    def trim_time_range
+      if range.first > subtract.first
+        subtract.last..range.last
+      else
+        range.first..subtract.first
+      end
+    end
+
+    attr_reader :range, :subtract
+  end
+end
