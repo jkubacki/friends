98275343bf34512eb506e5be607e3f19d0962ed8
Jakub Kubacki <kubacki.jk@gmail.com>
2019-02-17 20:04:55 -0500

    Add memberships and members associations to group

--- app/models/group.rb
+++ app/models/group.rb
@@ -2,6 +2,9 @@
 
 class Group < ApplicationRecord
   belongs_to :creator, class_name: "User"
+  has_many :memberships
+  has_many :members, through: :memberships, source: :user
+
   validates(
     :meeting_length_in_minutes, :creator_required, :min_users_in_meeting, :creator,
     presence: true
--- spec/models/group_spec.rb
+++ spec/models/group_spec.rb
@@ -4,6 +4,8 @@ require "rails_helper"
 
 RSpec.describe Group, type: :model do
   it { is_expected.to belong_to :creator }
+  it { is_expected.to have_many :memberships }
+  it { is_expected.to have_many(:members).through(:memberships) }
 
   it { is_expected.to validate_presence_of :meeting_length_in_minutes }
   it { is_expected.to validate_presence_of :creator_required }
